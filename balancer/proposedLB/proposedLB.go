92a969c8e45d716fec72d22013039db434da5038
meldyer1 <melaniedyer17@gmail.com>
2023-01-20 10:26:37 -0500

    performance improvements

--- balancer/proposedLB/proposedLB.go
+++ balancer/proposedLB/proposedLB.go
@@ -4,69 +4,64 @@ import (
 	"gonum.org/v1/gonum/optimize"
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
-	"google.golang.org/grpc/benchmark/stats"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/keepalive"
-	"math/rand"
 	"sync/atomic"
+	"time"
 )
 
 // Name taken from rr example
 const Name = "proposedLB"
 
-
-
 // initialize variables before calling functions which will change the values
-
-var numConnections = 0
 
 var growthFactor = 0.6
 
 // taken from rr example
 var logger = grpclog.Component("proposedLB")
 
-
 // taken from rr example
 func newProposedBuilder() balancer.Builder {
 	return base.NewBalancerBuilder(Name, &newLBPickerBuilder{extraParams: keepalive.ServerParameters{
 		MaxConnectionAgeGrace: time.Duration(10 / growthFactor)},
 		extraParams2: keepalive.EnforcementPolicy{
-		MinTime: time.Duration(1),
+			MinTime: time.Duration(1),
 		},
 	}, base.Config{HealthCheck: true})
 }
 
-
- 
 // taken from rr example
 func init() {
 	balancer.Register(newProposedBuilder())
 }
 
 type newLBPickerBuilder struct {
-	 extraParams keepalive.ServerParameters
-	 extraParams2 keepalive.EnforcementPolicy
+	extraParams  keepalive.ServerParameters
+	extraParams2 keepalive.EnforcementPolicy
 }
 
 func (*newLBPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 	// specify method used to choose the load balancer here
 	// lines below are taken from rr example
 	logger.Infof("proposedLB: Build called with info: %v", info)
+
 	if len(info.ReadySCs) == 0 {
 		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
 	}
-	
+
 	scs := make([]balancer.SubConn, 0, len(info.ReadySCs))
 
-	cg := optimize.LBFGS{
-	}
-	
 	arrNums := make([]float64, 0, len(info.ReadySCs))
-	
+
+	cg := optimize.CG{
+		Variant:      &optimize.HestenesStiefel{},
+		Linesearcher: &optimize.MoreThuente{},
+	}
+	//cg.Init(1, len(info.ReadySCs))
+
 	return &newlbPicker{
 		subConns: scs,
-		next: uint32(rand.Intn(len(scs))),
-		res: int(cg.NextDirection(optimize.Location{X: arrNums}, arrNums)),
+		next:     uint32(cg.NextDirection(&optimize.Location{}, arrNums)),
 	}
 }
 
@@ -76,19 +71,16 @@ type newlbPicker struct {
 	// select from it and return the selected SubConn.
 	// use grpc methods and avoid using third party libraries
 	subConns []balancer.SubConn
-		
-	next uint32
-	
-	res int
-	
-	}
 
-func (n *newlbPicker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
+	next uint32
+}
+
+func (n *newlbPicker) Pick(balancer.PickInfo) (balancer.PickResult, error) {
 	//Taken from rr algorithm
 	subConnsLen := uint32(len(n.subConns))
 
-	nextIndex := atomic.AddUint32(&n.res, 1)
+	nextIndex := atomic.AddUint32(&n.next, 1)
 
 	sc := n.subConns[nextIndex%subConnsLen]
-	return balancer.PickResult{SubConn: sc}, nil	
+	return balancer.PickResult{SubConn: sc}, nil
 }
