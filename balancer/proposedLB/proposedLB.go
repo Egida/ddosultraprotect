79d37da1ace102bf9a93184ae0c516b90fe675df
meldyer1 <melaniedyer17@gmail.com>
2023-01-30 15:14:07 -0500

    fastest algorithm to date

--- balancer/proposedLB/proposedLB.go
+++ balancer/proposedLB/proposedLB.go
@@ -2,6 +2,7 @@ package proposedLB
 
 import (
 	"gonum.org/v1/gonum/optimize"
+	"gonum.org/v1/gonum/stat"
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
 	"google.golang.org/grpc/grpclog"
@@ -19,6 +20,13 @@ const Name = "proposedLB"
 // taken from rr example
 var logger = grpclog.Component("proposedLB")
 
+//var cg = optimize.CG{
+//	Variant:                &optimize.HestenesStiefel{},
+//	Linesearcher:           &optimize.MoreThuente{},
+//	IterationRestartFactor: 1,
+//	GradStopThreshold:      0,
+//}
+
 // taken from rr example
 func newProposedBuilder() balancer.Builder {
 
@@ -34,11 +42,36 @@ func init() {
 type newLBPickerBuilder struct {
 }
 
+func objective(x []float64) float64 {
+	// Compute the loss function
+	x1 := x0(len(x))
+
+	loss := stat.ChiSquare(x1, x)
+
+	// Compute the L2 regularization term
+	reg := 0.0
+	for i := 0; i < len(x); i++ {
+		reg += x[i] * x[i]
+	}
+	reg *= rand.Float64() / 2.0
+
+	// Return the sum of the loss function and the regularization term
+	return loss + reg
+}
+
+func x0(l int) []float64 {
+	var arrN = make([]float64, 0, 0)
+	for i := 0; i < l; i++ {
+		arrN = append(arrN, rand.Float64())
+	}
+	return arrN
+}
+
 func (*newLBPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 	// specify method used to choose the load balancer here
 	// lines below are taken from rr example
 	logger.Infof("proposedLB: Build called with info: %v", info)
-	allSCs := len(info.ReadySCs)
+	allSCs := rand.Intn(len(info.ReadySCs))
 
 	if allSCs == 0 {
 		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
@@ -46,15 +79,13 @@ func (*newLBPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 
 	scs := make([]balancer.SubConn, allSCs)
 
-	cg := optimize.CG{
-		Variant:                &optimize.HestenesStiefel{},
-		Linesearcher:           &optimize.MoreThuente{},
-		IterationRestartFactor: 1,
-		GradStopThreshold:      0,
-	}
+	result, _ := optimize.Minimize(optimize.Problem{Func: objective}, x0(allSCs), nil, &optimize.CG{})
+
 	return &newlbPicker{
 		subConns: scs,
-		next:     uint32(cg.NextDirection(&optimize.Location{}, make([]float64, float64(rand.Intn(allSCs))))),
+		next:     uint32(result.F),
+
+		//cg.NextDirection(&optimize.Location{}, make([]float64, float64(rand.Intn(allSCs))))),
 	}
 }
 
